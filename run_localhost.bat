3cd1d95c81ae339536e1a4f33d1ba7d7996dcc86
lvettore02@gmail.com <lvettore02@gmail.com>
2026-05-20 12:03:08 +0200

    modifiche launcher windows

--- run_localhost.bat
+++ run_localhost.bat
@@ -11,14 +11,7 @@ set NOTEBOOK_NAME=interfaces/final_interfaces.ipynb
 
 echo [1/2] Ricerca e attivazione dell'ambiente Conda: %CONDA_ENV_NAME%...
 
-if exist "%USERPROFILE%\miniconda3\Scripts\activate.bat" (
-    call "%USERPROFILE%\miniconda3\Scripts\activate.bat" %CONDA_ENV_NAME%
-) else if exist "%USERPROFILE%\anaconda3\Scripts\activate.bat" (
-    call "%USERPROFILE%\anaconda3\Scripts\activate.bat" %CONDA_ENV_NAME%
-) else (
-    :: Fallback se conda e gia esposto globalmente nel prompt dei comandi
-    call conda activate %CONDA_ENV_NAME%
-)
+call conda activate %CONDA_ENV_NAME%
 
 :: Controllo se l'attivazione ha avuto successo
 if %errorlevel% neq 0 (
